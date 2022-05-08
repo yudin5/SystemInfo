0a59e1a7108262cf893bf19a3e04c2bb3a0702cb
yudin5 <yudin5@yandex.ru>
2022-05-09 00:36:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/systeminfo/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.systeminfo;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnInfo;
+    private TextView tvInfo;
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnInfo = findViewById(R.id.btn_info);
+        tvInfo = findViewById(R.id.tv_info);
+
+        btnInfo.setOnClickListener(v -> {
+            TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+            StringBuilder builder = new StringBuilder();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            builder.append("Info: ").append(tm.getLine1Number());
+            String text = builder.toString();
+            System.out.println("text = " + text);
+            tvInfo.setText(text);
+        });
+
+    }
+}
